f4b1c58f6c499f484ae398b23cac8ed4950cc0dd
bakerAllozi <bakerdes654@gmail.com>
2024-12-17 23:42:39 +0300

    add ts1

---
+++ src/Ts0.ts
@@ -0,0 +1,49 @@
+// let baker: boolean = true;
+// const names: string[] = "ss".split(" ");
+// const names1: Array<number> = [1, 2, 3];
+
+// const banzer: boolean = true;
+// const banzer1: string = "true";
+// const banzer2: number = 4;
+// const banzer3: Array<string> = ["1", "2", "10"];
+// const banzer4: Array<boolean> = [true, false];
+
+// interface baker3 {
+//   nameess: string;
+//   age: number;
+// }
+// const mybaker: { names: string; age: number } = {
+//   names: "bakorooo",
+//   age: 23,
+// };
+
+// const ids: Record<number, string> = {
+//   10: "a",
+//   20: "b",
+// };
+
+// ids[30] = "c";
+// function add(a: string, b: string): string {
+//   return a + b;
+// }
+
+// function add(t: string, p: string | number): string {
+//   return t + p;
+// }
+// console.log(add("8", "9"));
+//
+
+// function noretern(text: string): void {
+//   console.log(text);
+// }
+//--------------------------------------------------------------------------------------
+// function noretern(numbers: number, mutet: (v: number) => number): number[] {
+//   return numbers.map(mutet);
+// }
+// noretern([5, 7, 8], (v) => v * 5);
+
+// function noretern(numbers: number[], mutet: (v: number) => number): number[] {
+//   return numbers.map(mutet);
+// }
+
+// console.log(noretern([5, 7, 8], (v) => v * 5));
